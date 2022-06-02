e7fc7b5c34fb4316326774cc9e77e41c7a2549fd
Ubuntu <azureuser@open-domain-chatbot.zngtifkwcjludijllzlfemhrrf.hx.internal.cloudapp.net>
2022-06-02 02:42:37 +0000

    implement class begin logic and random choices

--- parlai/scripts/flask.py
+++ parlai/scripts/flask.py
@@ -20,10 +20,19 @@ curl -k http://localhost:5000/response -H "Content-Type: application/json" -d '{
 ```
 """
 
+
+import random
+from datetime import datetime
+random.seed(datetime.now())
+class_start_sentences=[" Okay, so are you ready for our class now?", " All right, are you ready for our lesson today?", 
+        " Ok, are you prepared for our lesson now?", " All right, shall we start our lesson now?"]
+
+
 from parlai.core.agents import create_agent
 from parlai.core.params import ParlaiParser
 from parlai.core.script import ParlaiScript, register_script
 from nltk.tokenize import sent_tokenize
+
 
 @register_script('flask', hidden=True)
 class Flask(ParlaiScript):
@@ -34,37 +43,49 @@ class Flask(ParlaiScript):
 
     def chatbot_response(self):
         from flask import request
-
+        # print("RECEIVED")
+        self.turn_cnt += 1
         data = request.headers.get("text")
-        self.agent.observe({'text': data["text"], 'episode_done': False})
+        self.agent.observe({'text': data, 'episode_done': False})
         response = self.agent.act()
+        # print(response)
+        rand_int = random.randint(0, 4)
+        response.force_set("text", response["beam_texts"][rand_int][0]);
         response_by_sent = sent_tokenize(response['text'])
         last_question = len(response_by_sent) - 1
         find_flag = False
+        class_begin = False
         for id, sent in reversed(list(enumerate(response_by_sent))):
             if sent.find("?") != -1:
                 last_question = id
                 find_flag = True
                 break
-        if find_flag == True:
+        if (self.turn_cnt >= 3 and find_flag == True and last_question != 0) or\
+                (self.turn_cnt >= 7 and find_flag == True):
             response.force_set("text", "")
-            response_by_sent[last_question] = "Okay, so are you ready for our class?"
+            response_by_sent[last_question] = random.choice(class_start_sentences) 
+            self.turn_cnt = 0
+            class_begin = True
             self.agent.reset()
             for id, s in enumerate(response_by_sent):
                 if id <= last_question:
-                    response.force_set('text', response["text"].join(s))
+                    # print(s)
+                    response.force_set('text', response["text"] + s)
                 else:
                     break
         # print(response)
-        return {'response': response['text']}
+        if class_begin:
+            return {'response': "[ClassBegin]" + response['text']}
+        else:
+            return {'response': response['text']}
 
     def run(self):
         from flask import Flask
-
+        self.turn_cnt = 0
         self.agent = create_agent(self.opt)
         app = Flask("parlai_flask")
         app.route("/response", methods=("GET", "POST"))(self.chatbot_response)
-        app.run(host='0.0.0.0', port=5000)
+        app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 if __name__ == "__main__":
